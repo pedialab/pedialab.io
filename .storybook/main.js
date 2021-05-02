c1a0a5e1074e94dab0d87927c27fcfd4a8d87c5c
Po-Huai Cheng <chengpohuai@perapera.ai>
2021-05-02 22:11:12 +0800

    feat: create index Page container

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  stories: ['../shared/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../shared/**/*.stories.@(js|jsx|ts|tsx)',
+    '../indexPage/*.stories.tsx',
+    '../caseStudyPage/*.stories.tsx'
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 };
