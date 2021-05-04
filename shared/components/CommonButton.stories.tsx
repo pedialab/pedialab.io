cc16986a03068d9a719f970d5c6fcf155617008f
Po-Huai Cheng <chengpohuai@perapera.ai>
2021-05-04 16:04:54 +0800

    feat: shared CommonButton component

---
+++ shared/components/CommonButton.stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react';
+import CommonButton, { CommonButtonProps } from './CommonButton';
+
+export default {
+  title: 'shared/CommonButton',
+  component: CommonButton
+} as Meta;
+
+const Template: Story<CommonButtonProps> = ({
+  title,
+  subtitle
+}: CommonButtonProps) => <CommonButton title={title} subtitle={subtitle} />;
+
+export const Default = Template.bind({});
+Default.args = { title: 'Let\'s Talk' };
+
+export const WithSubtitle = Template.bind({});
+WithSubtitle.args = {
+  ...Default.args,
+  subtitle: '*Budget in USD'
+};
