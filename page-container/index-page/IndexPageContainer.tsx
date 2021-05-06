8f210df372dc0262d0e3e6b142e28a11efcc2338
Po-Huai Cheng <chengpohuai@perapera.ai>
2021-05-06 17:52:02 +0800

    feat: index ProjectScopes component

--- page-container/index-page/IndexPageContainer.tsx
+++ page-container/index-page/IndexPageContainer.tsx
@@ -1,11 +1,7 @@
 import { Box, Grid, makeStyles } from '@material-ui/core';
 import { Header, Footer } from 'pedialab-shared/components';
 import {
-  HeroContent,
-  OurCompany,
-  SoftwareDelivery,
-  VendorDifference,
-  TechleadaaS
+  HeroContent, OurCompany, SoftwareDelivery, VendorDifference, TechleadaaS, ProjectScopes
 } from 'pedialab-pages/index-page';
 
 const useStyle = makeStyles({
@@ -24,6 +20,9 @@ const useStyle = makeStyles({
   },
   techleadaaS: {
     marginTop: '14.65rem'
+  },
+  projectScopes: {
+    marginTop: '15rem'
   },
   footer: {
     marginTop: '9.4rem'
@@ -47,6 +46,7 @@ const IndexPageContainer = () => {
         <TechleadaaS className={classes.techleadaaS} />
       </Box>
       <Box mx="10%">
+        <ProjectScopes className={classes.projectScopes} />
         <Footer className={classes.footer} />
       </Box>
     </>
