9c403827845ee45c530e7c92a8db5f94a1f5e558
PoHuai Cheng <chengpohuai@perapera.ai>
2021-05-12 15:41:11 +0800

    feat: add logo link to index page, replace HeroContent image

--- shared/components/LogoWithName.tsx
+++ shared/components/LogoWithName.tsx
@@ -1,13 +1,17 @@
-import { Box, Grid, Typography } from '@material-ui/core';
+import {
+  Box, Grid, Typography
+} from '@material-ui/core';
 import Logo from './Logo';
 
 const LogoWithName = () => (
-  <Grid container>
-    <Box mr="8px">
-      <Logo />
-    </Box>
-    <Typography variant="h1">Pedia Lab</Typography>
-  </Grid>
+  <a href="/">
+    <Grid container>
+      <Box mr="8px">
+        <Logo />
+      </Box>
+      <Typography variant="h1">Pedia Lab</Typography>
+    </Grid>
+  </a>
 );
 
 export default LogoWithName;
