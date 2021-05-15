e0f92d339078faa07f70179c4293fdfaf389dca9
PoHuai Cheng <chengpohuai@perapera.ai>
2021-05-16 04:12:13 +0800

    feat: mobile Footer

--- shared/components/Footer.tsx
+++ shared/components/Footer.tsx
@@ -1,25 +1,44 @@
-import { Typography, Box } from '@material-ui/core';
+import { Typography, Grid, makeStyles } from '@material-ui/core';
 import LogoWithName from './LogoWithName';
 
 export type FooterProps = {
   className?: string;
 };
 
-const Footer = ({ className }: FooterProps) => (
-  <Box
-    component="footer"
-    className={className}
-    borderTop={1}
-    paddingTop="4.4rem"
-    borderColor="#2F2F2F"
-  >
-    <LogoWithName />
-    <Box mt={2}>
-      <Typography variant="caption" component="small" color="textSecondary">
-        Singapore Company Entity, PeraPera Private Limited
-      </Typography>
-    </Box>
-  </Box>
-);
+const useStyles = makeStyles({
+  root: {
+    borderTopWidth: '1px',
+    borderTopStyle: 'solid',
+    borderTopColor: '#2F2F2F',
+    paddingTop: '4.375rem',
+    '& > :last-child': {
+      marginTop: '1.5rem',
+      marginBottom: '1rem'
+    }
+  },
+  logoContainer: {
+    width: 'max-content'
+  }
+});
+
+const Footer = ({ className }: FooterProps) => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      component="footer"
+      className={[className, classes.root].join(' ')}
+      direction="column"
+      alignItems="center"
+    >
+      <Grid item className={classes.logoContainer}><LogoWithName /></Grid>
+      <Grid item>
+        <Typography variant="caption" component="small" color="textSecondary">
+          Singapore Company Entity, PeraPera Private Limited
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Footer;
