a3db71bf956cb3157745c8a0083700cd359af728
PoHuai Cheng <chengpohuai@perapera.ai>
2021-05-16 15:58:29 +0800

    feat: mobile CaseStudiesPage

--- page-container/case-study-page/CaseStudiesPageContainer.tsx
+++ page-container/case-study-page/CaseStudiesPageContainer.tsx
@@ -4,12 +4,33 @@ import {
 import { Header, Footer, ArticleCardList } from 'pedialab-shared/components';
 import { ArticleCardProps } from 'pedialab-shared/components/ArticleCard';
 
-const useStyle = makeStyles({
-  caseStudyList: { marginTop: '5rem' },
+const useStyle = makeStyles((theme) => ({
+  title: {
+    marginTop: '9.63rem',
+    color: theme.palette.text.secondary,
+    fontSize: '3.75rem',
+    fontWeight: 300,
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '3.688rem',
+      fontSize: '2.25rem',
+      textAlign: 'center'
+    }
+  },
+  caseStudyList: {
+    marginTop: '5rem',
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '3.313rem',
+      paddingLeft: '5.55vw',
+      paddingRight: '5.55vw'
+    }
+  },
   footer: {
-    marginTop: '9.4rem'
+    marginTop: '9.4rem',
+    [theme.breakpoints.down('sm')]: {
+      marginTop: '5.313rem'
+    }
   }
-});
+}));
 
 const CaseStudiesPage = ({ articleCards }: { articleCards: ArticleCardProps[]}) => {
   const classes = useStyle();
@@ -17,14 +38,12 @@ const CaseStudiesPage = ({ articleCards }: { articleCards: ArticleCardProps[]}) 
     <Box mx={{ xs: '5.8%', md: '10%' }}>
       <Grid container direction="column">
         <Header isActivatedOrder={3} />
-        <Box
-          mt="9.63rem"
-          color="text.secondary"
-          fontSize="3.75rem"
-          fontWeight={300}
+        <Grid
+          item
+          className={classes.title}
         >
           Case Studies
-        </Box>
+        </Grid>
         <ArticleCardList articleCards={articleCards} className={classes.caseStudyList} />
         <Footer className={classes.footer} />
       </Grid>
